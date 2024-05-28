115e7db9faa76c5d32e8076903f3d57043aeda20
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-05-29 01:36:29 +0200

    fix mypy error

--- server/main.py
+++ server/main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Response
+from fastapi.responses import JSONResponse
 from routers.captcha import router as captcha_router
 from database import models
 from database.core import engine
-from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from routers.limiter import limiter
 from config.settings import settings
@@ -14,4 +14,15 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(captcha_router)
 
 app.state.limiter = limiter
-app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
+
+
+# TODO: This is an ugly (?) workaround to make mypy happy
+# I would like to use:
+# from slowapi import _rate_limit_exceeded_handler
+# But mypy raises an error: Argument 2 to "add_exception_handler" of "Starlette" has incompatible type...
+# If any strange behavior occurs, this is the first place to look
+async def rate_limit_exceeded_handler(request: Request, exc: Exception) -> Response:
+    return JSONResponse(status_code=429, content={"detail": "Too Many Requests"})
+
+
+app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
