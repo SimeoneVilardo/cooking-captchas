b50a9b560bb68d87590ff71fafaf7ad2c614a131
SimeoneVilardo <simeone.vilardo@gmail.com>
2024-05-28 20:03:37 +0200

    refactor table columns

--- server/database/models.py
+++ server/database/models.py
@@ -1,12 +1,13 @@
-import datetime
-from sqlalchemy import Boolean, Column, DateTime, Integer, String
+from datetime import datetime
+from sqlalchemy import Boolean, DateTime, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 from database.core import Base
 
 
 class DBCaptcha(Base):
     __tablename__ = "captchas"
 
-    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
-    value = Column(String, index=True)
-    is_used = Column(Boolean, default=False)
-    created_at = Column(DateTime, default=datetime.datetime.now)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True, autoincrement=True)
+    value: Mapped[str] = mapped_column(String, index=True)
+    is_used: Mapped[bool] = mapped_column(Boolean, default=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
