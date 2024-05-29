0bef7aea13df6ffad4be86840cc9688c81e9afc5
Simeone Vilardo <simeone.vilardo@vedrai.com>
2024-05-29 11:10:18 +0200

    fix db first connection

--- server/main.py
+++ server/main.py
@@ -1,28 +1,37 @@
+import time
 from fastapi import FastAPI, Request, Response
 from fastapi.responses import JSONResponse
 from routers.captcha import router as captcha_router
 from database import models
 from database.core import engine
 from slowapi.errors import RateLimitExceeded
+from slowapi import _rate_limit_exceeded_handler
 from routers.limiter import limiter
 from config.settings import settings
+from sqlalchemy.exc import OperationalError
+
 
 app = FastAPI(openapi_url=settings.openapi_url)
 
-models.Base.metadata.create_all(bind=engine)
+#TODO: Investigate better way to wait for db (maybe https://github.com/vishnubob/wait-for-it)
+max_retries = 10
+attempt = 0
+
+while attempt < max_retries:
+    try:
+        models.Base.metadata.create_all(bind=engine)
+        break  # Exit the loop if successful
+    except OperationalError:
+        attempt += 1
+        if attempt < max_retries:
+            time.sleep(1)
+    except Exception:
+        attempt += 1
+        if attempt < max_retries:
+            time.sleep(1)
 
 app.include_router(captcha_router)
 
 app.state.limiter = limiter
 
-
-# TODO: This is an ugly (?) workaround to make mypy happy
-# I would like to use:
-# from slowapi import _rate_limit_exceeded_handler
-# But mypy raises an error: Argument 2 to "add_exception_handler" of "Starlette" has incompatible type...
-# If any strange behavior occurs, this is the first place to look
-async def rate_limit_exceeded_handler(request: Request, exc: Exception) -> Response:
-    return JSONResponse(status_code=429, content={"detail": "Too Many Requests"})
-
-
-app.add_exception_handler(RateLimitExceeded, rate_limit_exceeded_handler)
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler) # type: ignore[arg-type]
